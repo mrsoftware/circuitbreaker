d9c44bf994db7171c2efccc6c3f30a1a4a0d34c3
Mohammad Rajabloo <themrsoftware@gmail.com>
2023-10-05 15:01:02 +0330

    refactor memory storage

--- memory.go
+++ memory.go
@@ -27,18 +27,13 @@ type MemoryStorage struct {
 	options     StorageOptions
 	failures    atomic.Int64
 	success     atomic.Int64
-	state       atomic.Int64
 	lastErrorAt atomic.Value
 }
 
 // Failure is responsible to store failures.
 func (m *MemoryStorage) Failure(ctx context.Context, delta int64) error {
 	m.lastErrorAt.Store(time.Now().UTC())
-
-	// open the circuit if we got too many failure.
-	if m.failures.Add(delta) >= m.options.FailureRateThreshold {
-		m.state.Store(int64(StateOpen))
-	}
+	m.failures.Add(delta)
 
 	return nil
 }
@@ -49,28 +44,30 @@ func (m *MemoryStorage) Success(ctx context.Context, delta int64) error {
 		return m.Reset(ctx)
 	}
 
-	lastErrorAt := m.lastErrorAt.Load().(time.Time)
-	errorExpireTTL := lastErrorAt.Add(m.options.OpenWindow).Sub(time.Now().UTC())
-	if errorExpireTTL <= 0 {
-		return m.Reset(ctx)
-	}
-
-	// are we in halfOpen window?
-	if errorExpireTTL <= m.options.HalfOpenWindow {
-		m.state.Store(int64(StateHalfOpen))
-	}
-
 	return nil
 }
 
 // GetState current state.
 func (m *MemoryStorage) GetState(ctx context.Context) (State, error) {
-	return State(m.state.Load()), nil
+	lastErrorAt := m.lastErrorAt.Load().(time.Time)
+	errorExpireTTL := lastErrorAt.Add(m.options.OpenWindow).Sub(time.Now().UTC())
+	if errorExpireTTL <= 0 {
+		return StateClose, m.Reset(ctx)
+	}
+
+	if errorExpireTTL <= m.options.HalfOpenWindow {
+		return StateHalfOpen, nil
+	}
+
+	if m.failures.Load() >= m.options.FailureRateThreshold {
+		return StateOpen, nil
+	}
+
+	return StateClose, nil
 }
 
 // Reset the state.
 func (m *MemoryStorage) Reset(ctx context.Context) error {
-	m.state.Store(int64(StateClose))
 	m.success.Store(0)
 	m.failures.Store(0)
 	m.lastErrorAt.Store(time.Time{})
--- memory_test.go
+++ memory_test.go
@@ -16,80 +16,70 @@ func TestMemoryStorageStorage_Failure(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, int64(5), ms.failures.Load())
 		assert.Equal(t, time.Now().UTC().Minute(), ms.lastErrorAt.Load().(time.Time).Minute())
-		assert.Equal(t, StateClose, State(ms.state.Load()))
-		assert.Equal(t, int64(0), ms.success.Load())
-	})
-
-	t.Run("expected to increment the failure count and last error at and change state to close", func(t *testing.T) {
-		ms := NewMemoryStorage(WithFailureRateThreshold(5))
-
-		err := ms.Failure(context.Background(), 10)
-		assert.Nil(t, err)
-		assert.Equal(t, int64(10), ms.failures.Load())
-		assert.Equal(t, time.Now().UTC().Minute(), ms.lastErrorAt.Load().(time.Time).Minute())
-		assert.Equal(t, StateOpen, State(ms.state.Load()))
 		assert.Equal(t, int64(0), ms.success.Load())
 	})
 }
 
 func TestMemoryStorageStorage_Success(t *testing.T) {
-	t.Run("state is close, expect to do nothing", func(t *testing.T) {
-		ms := NewMemoryStorage()
-
-		err := ms.Success(context.Background(), 1)
-		assert.Nil(t, err)
-		assert.Equal(t, int64(0), ms.success.Load())
-		assert.Equal(t, int64(0), ms.failures.Load())
-		assert.Equal(t, StateClose, State(ms.state.Load()))
-		assert.Equal(t, 0, ms.lastErrorAt.Load().(time.Time).Minute())
-	})
-
-	t.Run("state is open, expect to only increment the success count", func(t *testing.T) {
+	t.Run("expect to only increment the success count", func(t *testing.T) {
 		ms := NewMemoryStorage(WithOpenWindow(1*time.Minute), WithSuccessRateThreshold(2))
-		ms.state.Store(int64(StateOpen))
-		ms.lastErrorAt.Store(time.Now().UTC())
+		now := time.Now().UTC()
+		ms.lastErrorAt.Store(now)
 
 		err := ms.Success(context.Background(), 1)
 		assert.Nil(t, err)
 		assert.Equal(t, int64(1), ms.success.Load())
 		assert.Equal(t, int64(0), ms.failures.Load())
-		assert.Equal(t, StateOpen, State(ms.state.Load()))
-		assert.Equal(t, time.Now().UTC().Minute(), ms.lastErrorAt.Load().(time.Time).Minute())
+		assert.Equal(t, now, ms.lastErrorAt.Load().(time.Time))
 	})
 
-	t.Run("state is open, change state to close if last error happened befor openwindow", func(t *testing.T) {
-		ms := NewMemoryStorage(WithSuccessRateThreshold(10), WithOpenWindow(5*time.Minute))
-		ms.state.Store(int64(StateOpen))
-		ms.lastErrorAt.Store(time.Now().UTC().Add(-5 * time.Minute))
+	t.Run("expect to increment the success count and reset circuit", func(t *testing.T) {
+		ms := NewMemoryStorage(WithOpenWindow(1*time.Minute), WithSuccessRateThreshold(1))
+		now := time.Now().UTC()
+		ms.lastErrorAt.Store(now)
+		ms.failures.Store(3)
 
-		err := ms.Success(context.Background(), 2)
+		err := ms.Success(context.Background(), 1)
 		assert.Nil(t, err)
 		assert.Equal(t, int64(0), ms.success.Load())
 		assert.Equal(t, int64(0), ms.failures.Load())
-		assert.Equal(t, StateClose, State(ms.state.Load()))
-		assert.Equal(t, 0, ms.lastErrorAt.Load().(time.Time).Minute())
-	})
-
-	t.Run("state is open and we in the half open window, expect to change state to half open", func(t *testing.T) {
-		ms := NewMemoryStorage(WithSuccessRateThreshold(10), WithOpenWindow(6*time.Minute), WithHalfOpenWindow(3*time.Minute))
-
-		ms.state.Store(int64(StateOpen))
-		lastErrAt := time.Now().UTC().Add(-ms.options.HalfOpenWindow)
-		ms.lastErrorAt.Store(lastErrAt)
-
-		err := ms.Success(context.Background(), 2)
-		assert.Nil(t, err)
-		assert.Equal(t, int64(2), ms.success.Load())
-		assert.Equal(t, int64(0), ms.failures.Load())
-		assert.Equal(t, StateHalfOpen, State(ms.state.Load()))
-		assert.Equal(t, lastErrAt, ms.lastErrorAt.Load().(time.Time))
+		assert.Equal(t, time.Time{}, ms.lastErrorAt.Load().(time.Time))
 	})
 }
 
 func TestMemorystorage_getstatus(t *testing.T) {
-	t.Run("expected to return the stored state and do nothing", func(t *testing.T) {
-		ms := NewMemoryStorage()
-		ms.state.Store(int64(StateClose))
+	t.Run("the last error is expired, expect to reset the circuit", func(t *testing.T) {
+		ms := NewMemoryStorage(WithOpenWindow(10 * time.Minute))
+		ms.lastErrorAt.Store(time.Now().UTC().Add(-11 * time.Minute))
+
+		cState, err := ms.GetState(context.Background())
+		assert.Nil(t, err)
+		assert.Equal(t, StateClose, cState)
+	})
+
+	t.Run("we are in the half open state based on the last error time", func(t *testing.T) {
+		ms := NewMemoryStorage(WithOpenWindow(10*time.Minute), WithHalfOpenWindow(5*time.Minute))
+		ms.lastErrorAt.Store(time.Now().UTC().Add(-5 * time.Minute))
+
+		cState, err := ms.GetState(context.Background())
+		assert.Nil(t, err)
+		assert.Equal(t, StateHalfOpen, cState)
+	})
+
+	t.Run("circuit is not expired and we are not in the half open state, but the error threshold is reached", func(t *testing.T) {
+		ms := NewMemoryStorage(WithOpenWindow(10*time.Minute), WithHalfOpenWindow(5*time.Minute), WithFailureRateThreshold(2))
+		ms.lastErrorAt.Store(time.Now().UTC().Add(1 * time.Minute))
+		ms.failures.Store(2)
+
+		cState, err := ms.GetState(context.Background())
+		assert.Nil(t, err)
+		assert.Equal(t, StateOpen, cState)
+	})
+
+	t.Run("circuit is not expired and we are not in the half open state, and err threshold is not reached, so the state is close", func(t *testing.T) {
+		ms := NewMemoryStorage(WithOpenWindow(10*time.Minute), WithHalfOpenWindow(5*time.Minute), WithFailureRateThreshold(2))
+		ms.lastErrorAt.Store(time.Now().UTC().Add(1 * time.Minute))
+		ms.failures.Store(1)
 
 		cState, err := ms.GetState(context.Background())
 		assert.Nil(t, err)
@@ -98,7 +88,7 @@ func TestMemorystorage_getstatus(t *testing.T) {
 }
 
 func TestMemoryStorageStorage_Reset(t *testing.T) {
-	t.Run("exptecte to set failure, success, last error at to default value (0)", func(t *testing.T) {
+	t.Run("expected to set failure, success, last error at to default value (0)", func(t *testing.T) {
 		ms := NewMemoryStorage()
 		ms.failures.Store(20)
 		ms.success.Store(10)
@@ -108,7 +98,6 @@ func TestMemoryStorageStorage_Reset(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, int64(0), ms.success.Load())
 		assert.Equal(t, int64(0), ms.failures.Load())
-		assert.Equal(t, StateClose, State(ms.state.Load()))
 		assert.Equal(t, time.Time{}, ms.lastErrorAt.Load().(time.Time))
 	})
 }
